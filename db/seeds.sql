76e0e739021ebbe6df89601c7488837cf30d567f
rsa153 <r-saber@nlaw.northwestern.edu>
2019-04-14 21:10:17 -0500

    test

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ("turkey", true);
+INSERT INTO burgers (name, devoured) VALUES ("beef", false);
+INSERT INTO burgers (name, devoured) VALUES ("elk", true);
