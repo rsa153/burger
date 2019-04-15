9f10a9c31563ffdbb8baa8298835a73cfd537f33
rsa153 <r-saber@nlaw.northwestern.edu>
2019-04-14 21:45:53 -0500

    db test

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "127.0.0.1",
+  host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
+  password: "",
   database: "burger_db"
 });
 
