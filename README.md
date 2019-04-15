4d225297c18db6a81ba9f02b9bc2ccc721968616
rsa153 <r-saber@nlaw.northwestern.edu>
2019-04-14 23:40:07 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# burger
+# Burger Time
+
+### What is Burger Time?
+Burger Time is a restaurant app that allows patrons to track the burgers that they would like to eat and that they've eaten. Users can input the names of the burgers that they would like to eat. By default,the app will assume that the patron has not yet devoured the burger and will display it under the "Burgers that have NOT been devoured!" section of the app. The patron can then choose to delete the burger (if they inputted it incorrectly) or devour it. Once a burger is devoured, it will appear in the "Burgers that have been devoured!" section. 
+
+This app is built using express, handlebars, MYSQL, javascript, bootstrap and CSS.
+
+
+### How do I get started with Burger Time?
+To use the app, please visit: https://rs-burger.herokuapp.com/. 
--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -17,14 +17,14 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "name", "devoured"
-  ], [
-    req.body.name, req.body.devoured
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+  burger.create(
+    ["name", "devoured"],
+    [req.body.name, req.body.devoured],
+    function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    }
+  );
 });
 
 router.put("/api/burgers/:id", function(req, res) {
@@ -32,15 +32,19 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
+  burger.update(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
     }
-  });
+  );
 });
 
 router.delete("/api/burgers/:id", function(req, res) {
