da450d6e0caed00e82f8720eac8542599995a6d1
rsa153 <r-saber@nlaw.northwestern.edu>
2019-04-28 19:07:09 -0500

    missing notes

--- models/burger.js
+++ models/burger.js
@@ -7,7 +7,6 @@ var burger = {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
     orm.create("burgers", cols, vals, function(res) {
       cb(res);
